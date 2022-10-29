1a325a2422687f8f4614daeabc601a998dd5ff97
Josephkagimu1 <kagimujoseph00@gmail.com>
2022-10-29 17:56:22 +0300

    part-3

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import Task from './components/Task';
 
 function App() {
   const [task, setTask] = useState('');
   const [taskList, setTaskList] = useState([]);
+  const [editId, setEditId] = useState(0);
 
   //adding Task
-  function AddTask(){
+  function AddTask() {
     if (task !== "") {
-    setTaskList([ ...taskList, {task:task} ]);
-    setTask('');
+      setTaskList([...taskList, { task, index: `${task}-${Date.now()}` }]);
+      setTask('');
+    }
+
+    if (editId) {
+      const editTask = taskList.find((i) => i.index === editId);
+      const updatedTasks = taskList.map((t) => 
+        t.index === editTask.index ? t= {index: t.index , task} : {index: t.index, task: t.task}
+      )
+      setTaskList(updatedTasks);
+      setEditId(0);
+      return;
     }
   }
 
-   // deleting Todo .
+  // deleting Todo .
   const deleteTodo = (index) => {
-  var newList = taskList;
-  newList.splice(index,1);
-  setTaskList([...newList]);
-}
+    var newList = taskList;
+    newList.splice(index, 1);
+    setTaskList([...newList]);
+  }
+
+  const handleEdit = (index) => {
+    const editTask = taskList.find((i) => i.index === index);
+    setTask(editTask.task);
+    setEditId(index);
+  }
 
   return (
     <div className="App">
       <h1 className='title'> Todo App </h1>
-     <input type='text' value={task} onChange={(e) => {setTask(e.target.value)}} />
-     <button onClick={AddTask}> Add </button>
+      <input type='text' value={task} onChange={(e) => { setTask(e.target.value) }} />
+      <button onClick={AddTask}> Add </button>
 
-     {taskList.map((task , index) => {
-       return <Task task={task.task} deleteTodo={deleteTodo} index={index} />
-     } )}
+      {taskList.map((task) => {
+        return <Task task={task.task} deleteTodo={deleteTodo} index={task.index} handleEdit={handleEdit} />
+      })}
 
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Task = ({task , deleteTodo , index}) => {
+const Task = ({task , deleteTodo , index, handleEdit}) => {
 
   return (
     <div className='task'>
         <h2> {task} </h2>
-        <button className='delete-btn' onClick={() => deleteTodo(index)}> X </button>
+        <button className='delete-btn' onClick={() => deleteTodo(index)}> Delete </button>
+        <button className='edit-btn' onClick={() => handleEdit(index)}> Edit </button>
+
     </div>
   )
 }
